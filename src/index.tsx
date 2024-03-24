a6d36f3b10ba9f95386472ff48072b9d1c149b9d
Logan <logan.zartman@utexas.edu>
2024-03-23 19:38:28 -0700

    fix types

--- src/index.tsx
+++ src/index.tsx
@@ -3,5 +3,6 @@ import {createRoot} from 'react-dom/client';
 import App from '@/src/App';
 
 const container = document.getElementById('app');
+if (!container) throw new Error('No container element found');
 const root = createRoot(container);
 root.render(<App />);
